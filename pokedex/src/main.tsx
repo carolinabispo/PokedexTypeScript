9bc805af2a4701338f73d59ceec28dc7cb96586c
carolinabispo <carolinabispos07@gmail.com>
2023-08-10 19:51:48 -0300

    contexts fuctions aplications
    criação dos contexts da aplicação, contexts fuctions aplications

---
+++ pokedex/src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import PokemonProvider from './context/PokemonContext.js'
+
+const rootElement = document.getElementById('root')
+if(rootElement){
+  ReactDOM.createRoot(rootElement).render(
+    <PokemonProvider>
+        <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    </PokemonProvider>
+  
+  )
+}else{
+  console.log('root não existe');
+  
+}
